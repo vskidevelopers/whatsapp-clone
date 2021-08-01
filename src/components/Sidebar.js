1f3f234a639dce568c256e5e9326f4f0eb7b36e7
Ben <71015535+vskidevelopers@users.noreply.github.com>
2021-08-01 23:27:25 +0300

    first Commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYdPZbGOuvg8gb7le5bnG_kJTnlcmT90M",
+  authDomain: "whatsapp-clone-b9e60.firebaseapp.com",
+  projectId: "whatsapp-clone-b9e60",
+  storageBucket: "whatsapp-clone-b9e60.appspot.com",
+  messagingSenderId: "909706797018",
+  appId: "1:909706797018:web:57df43e6b358114385eae3",
+  measurementId: "G-88X4HVBLLW",
+};
